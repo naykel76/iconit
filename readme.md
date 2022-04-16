cb27d95bb3a1361ec9578377080164ce9e460d81
naykel76 <32632005+naykel76@users.noreply.github.com>
2022-04-17 05:35:51 +1000

    initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+<p align="center"><a href="https://naykel.com.au" target="_blank"><img src="https://avatars0.githubusercontent.com/u/32632005?s=460&u=d1df6f6e0bf29668f8a4845271e9be8c9b96ed83&v=4" width="120"></a></p>
+
+<a href="https://packagist.org/packages/naykel/iconit"><img src="https://img.shields.io/packagist/dt/naykel/iconit" alt="Total Downloads"></a>
+<a href="https://packagist.org/packages/naykel/iconit"><img src="https://img.shields.io/packagist/v/naykel/iconit" alt="Latest Stable Version"></a>
+<a href="https://packagist.org/packages/naykel/iconit"><img src="https://img.shields.io/packagist/l/naykel/iconit" alt="License"></a>
+
+# Iconit
+
+Icon components for NAYKEL Laravel applications
+
+
+
+
+
+## Command Line
+
+### change extension from `svg` to `blade.php`
+
+```bash
+for i in *.svg; do mv -- "$i" "${i%.svg}.blade.php"; done
+```
---
+++ src/IconitServiceProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Naykel\Iconit;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
+class IconitServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'iconit');
+        $this->configureComponents();
+    }
+
+    /**
+     * Configure the Blade components.
+     *
+     * @return void
+     */
+    protected function configureComponents()
+    {
+
+        // $this->registerComponent('naykel');
+        $this->createComponentsFromDirectory(); // base directory
+        $this->createComponentsFromDirectory('payment');
+        $this->createComponentsFromDirectory('logos');
+    }
+
+    /**
+     * Create component from directory files
+     *
+     * @param mixed $dir leave blank for components directory
+     * @return void
+     */
+
+    protected function createComponentsFromDirectory($dir = '')
+    {
+
+        $filesInFolder = \File::files(__dir__ . "/../resources/views/components/$dir");
+
+        foreach ($filesInFolder as $path) {
+            $fileInfo = pathinfo($path);
+
+            // for reasons I do not understand you can not strip
+            // the period from the components root directory so it
+            // needs to be removed separately
+            $component = rtrim($fileInfo['filename'], 'blade');
+            $component = rtrim($component, '.');
+
+            $this->registerComponent($component, $dir);
+        }
+    }
+
+    /**
+     * Register the given component.
+     *
+     * @param  string  $component
+     * @param  string  $dir component directory
+     * @param  string  $prefix (prefix.component)
+     * @return void
+     */
+    protected function registerComponent(string $component, string $dir = '', string $prefix = 'iconit.')
+    {
+        if (!empty($dir)) {
+            $dir = ".$dir";
+        }
+
+        Blade::component("iconit::components" . "$dir.$component", $prefix . $component);
+    }
+}
