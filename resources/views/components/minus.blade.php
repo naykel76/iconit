e100bd78be138f890bf595256ab216c8e8f4601d
Nathan <nathan@naykel.com.au>
2022-11-08 03:47:23 +1000

    add icons

---
+++ resources/views/components/minus.blade.php
@@ -0,0 +1,1 @@
+<svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" width="20" height="0.952" viewBox="0 0 20 0.952"><rect width="20" height="0.952"/></svg>
---
+++ resources/views/components/plus.blade.php
@@ -0,0 +1,1 @@
+<svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M20,9.524H10.476V0H9.524V9.524H0v.952H9.524V20h.952V10.476H20Z"/></svg>
