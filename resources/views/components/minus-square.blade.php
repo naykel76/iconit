5058525d59bc55f89be44b515f2e1b02c0de7761
Nathan <nathan@naykel.com.au>
2022-11-30 06:45:59 +1000

    add icons

---
+++ resources/views/components/minus-square.blade.php
@@ -0,0 +1,1 @@
+<svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M0,0V20H20V0ZM17,10.667H3V9.333H17Z"/></svg>
---
+++ resources/views/components/plus-square.blade.php
@@ -0,0 +1,1 @@
+<svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M0,0V20H20V0ZM17,10.667H10.667V17H9.333V10.667H3V9.333H9.333V3h1.333V9.333H17Z"/></svg>
